217d1ef9863d591ed0522da40909877d173eb195
Lukin <lukasz@coders.school>
2024-02-01 19:05:50 +0100

    Add adding

---
+++ main.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result = num1 + num2
+    print("The sum of", num1, "and", num2, "is:", result)
