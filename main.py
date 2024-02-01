efdeb987ee051b34fcbc332a3de835395578666a
Lukin <lukasz@coders.school>
2024-02-01 19:06:30 +0100

    Extract add_numbers() function
    Fixes #1, #2

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-if __name__ == "__main__":
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+def calc():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
-    result = num1 + num2
+    result = add_numbers(num1, num2)
     print("The sum of", num1, "and", num2, "is:", result)
+
+
+if __name__ == "__main__":
+    calc()
